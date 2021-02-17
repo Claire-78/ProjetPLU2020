1d98a52874fbbdc8d0e38fbb6e351a785cb337a3
Claire <claire.trinquet@eleves.ec-nantes.fr>
2021-02-17 16:00:59 +0100

    Création classe Polygone

---
+++ mavenproject1/src/main/java/org/centrale/projet/objet/Polygone.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.projet.objet;
+
+import java.util.*;
+/**
+ *
+ * @author clair
+ */
+public class Polygone {
+    private List<Point2D> sommets;
+    
+    Polygone(){
+        sommets = new ArrayList();
+    }
+    
+    Polygone(List<Point2D> points){
+        sommets = points;
+    }
+    
+    Polygone(Polygone p){
+        sommets = p.getSommets();
+    }
+    
+    public List<Point2D> getSommets(){
+        return sommets;
+    }
+    
+    public void setSommets(List<Point2D> points){
+        sommets = points;
+    }
+    
+    public void addPoint(Point2D p){
+        sommets.add(p);
+    }
+    
+    public void translate(int a, int b){
+        sommets.forEach(sommet -> {
+            sommet.translate(a,b);
+        });
+    }
+    
+    public float getSurface(){
+        float surface = 0;
+        Point2D pi;
+        Point2D pi1;
+        for (int i=0;i<sommets.size();i++){
+            pi = sommets.get(i);
+            pi1 = sommets.get(i%sommets.size());
+            surface += (float) (pi.getX()*pi1.getY() - pi1.getX()*pi.getY());
+        }
+        return (surface/2.0f);
+    }
+    
+    public String toString(){
+        String affichage = new String();
+        affichage += "Polygone à " + sommets.size() + " points :\n";
+        
+        for (Point2D sommet : sommets) {
+            affichage += sommet.toString() + "\n";
+        }
+        return affichage;
+    }
+    
+}

7ba702c7d1a5888b1be4075fd2249f1f3cac31ff
Claire <claire.trinquet@eleves.ec-nantes.fr>
2021-02-17 12:13:42 +0100

    Création de Point2D

---
+++ mavenproject1/src/main/java/org/centrale/projet/objet/Point2D.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.projet.objet;
+
+/**
+ *
+ * @author clair
+ */
+public class Point2D {
+    int x;
+    int y;
+    
+    Point2D(){
+        x = 0;
+        y = 0;
+    }
+    
+    Point2D(int a, int b){
+        x = a;
+        y = b;
+    }
+    
+    Point2D(Point2D p){
+        x = p.x;
+        y = p.y;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+    
+    public void setX(int a){
+        x = a;
+    }
+    
+    public void setY(int b){
+        x = b;
+    }
+    
+    public void setPosition(int a, int b){
+        x = a;
+        y = b;
+    }
+    
+    public void translate(int a, int b){
+        x += a;
+        y += b;
+    }
+    
+    public String toString(){
+        return("["+x+","+y+"]");
+    }
+    
+    public void affiche(){
+        System.out.println(toString());
+    }
+
+}

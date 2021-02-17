f94c4919b711e0d1341c4d39dabf2ae5fddaeae9
Claire <claire.trinquet@eleves.ec-nantes.fr>
2021-02-17 15:01:53 +0100

    Création de la classe PLU pour les tests

---
+++ mavenproject1/src/main/java/org/centrale/projet/objet/PLU.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.centrale.projet.objet;
+
+/**
+ *
+ * @author clair
+ */
+public class PLU {
+    public static void main(String[] args) {
+        Map map = new Map();
+        
+        map.creeMapAlea();
+        
+        map.affiche();
+    }
+}

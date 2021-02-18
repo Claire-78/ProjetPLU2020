d2a7101d5339d5210cd49b92471dacaff635ec76
Claire <claire.trinquet@eleves.ec-nantes.fr>
2021-02-18 12:03:43 +0100

    Affichage m²

--- mavenproject1/src/main/java/org/centrale/projet/objet/ZU.java
+++ mavenproject1/src/main/java/org/centrale/projet/objet/ZU.java
@@ -45,8 +45,8 @@ public class ZU extends ZAU {
 
     public String toString() {
         String s = super.toString();
-        s += "  Surface construite : " + df.format(surfaceConstruite) + "\n";
-        s += "  Surface constructible restante : " + df.format(surfaceConstructible()) + "\n";
+        s += "  Surface construite : " + df.format(surfaceConstruite) + "m²\n";
+        s += "  Surface constructible restante : " + df.format(surfaceConstructible()) + "m²\n";
         return s;
     }
 }
